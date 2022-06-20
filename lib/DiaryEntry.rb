5a116a257df62c3ac6c9480c3628d8a6db46f702
olliefreeman94 <oliver.freeman@yahoo.com>
2022-06-20 11:27:02 +0100

    Update Section 5 Exercise

--- lib/DiaryEntry.rb
+++ lib/DiaryEntry.rb
@@ -2,7 +2,7 @@ class DiaryEntry
   def initialize(title, contents)
     @title = title
     @contents = contents
-    @next_word = 0
+    @next_index = 0
   end
 
   def title
@@ -14,36 +14,37 @@ class DiaryEntry
   end
 
   def count_words
-    words = @contents.split(" ")
     return words.length
   end
 
   def reading_time(wpm)
+    if wpm <= 0
+      fail "Reading speed must be greater than zero."
+    end
     word_count = count_words
     return (word_count.to_f / wpm).ceil
   end
 
   def reading_chunk(wpm, minutes)
-    total_time = reading_time(wpm)
-    if total_time <= minutes
-      return @contents
+    if wpm <= 0 || minutes <= 0
+      fail "Reading speed and minutes must be greater than zero."
     end
-
-    word_count = count_words
-    words = @contents.split(" ")
     readable_words = wpm * minutes
-    if @next_word == 0
-      chunk = words[0..readable_words - 1]
-      @next_word += chunk.length
-      return chunk.join(" ")
-    elsif @next_word + readable_words < word_count
-      chunk = words[@next_word..(@next_word + readable_words - 1)]
-      @next_word += chunk.length
+
+    if readable_words >= words.length - @next_index
+      chunk = words[@next_index...words.length]
+      @next_index = 0
       return chunk.join(" ")
     else
-      chunk = words[@next_word..word_count - 1]
-      @next_word = 0
+      chunk = words[@next_index...@next_index + readable_words]
+      @next_index += readable_words
       return chunk.join(" ")
     end
+  end
+
+  private
+  
+  def words
+    return @contents.split(" ")
   end
 end
--- spec/DiaryEntry_spec.rb
+++ spec/DiaryEntry_spec.rb
@@ -11,73 +11,94 @@ RSpec.describe DiaryEntry do
     expect( entry.contents ).to eq "notes"
   end
 
-  context "given diary entries with contents of varying length" do
-    it "returns word count zero when contents is an empty string" do
-      entry = DiaryEntry.new("meeting", "")
-      expect( entry.count_words ).to eq 0
-    end
+  describe "count_words" do
+    context "given diary entries with contents of varying length" do
+      it "returns word count zero when contents is an empty string" do
+        entry = DiaryEntry.new("meeting", "")
+        expect( entry.count_words ).to eq 0
+      end
 
-    it "returns word count one when contents is a one word string" do
-      entry = DiaryEntry.new("meeting", "notes")
-      expect( entry.count_words ).to eq 1
-    end
+      it "returns word count one when contents is a one word string" do
+        entry = DiaryEntry.new("meeting", "notes")
+        expect( entry.count_words ).to eq 1
+      end
 
-    it "returns correct word count when contents is multi word string" do
-      entry = DiaryEntry.new("meeting", "notes " * 10)
-      expect( entry.count_words ).to eq 10
-    end
-  end
-
-  context "given varying reading speeds, returns appropriate reading time for given contents" do
-    it "returns 1 for reading speed 200 wpm, word count 100" do
-      entry = DiaryEntry.new("meeting", "notes " * 100)
-      expect( entry.reading_time(200) ).to eq 1
-    end
-
-    it "returns 5 for reading speed 200 wpm, word count 1000" do
-      entry = DiaryEntry.new("meeting", "notes " * 1000)
-      expect( entry.reading_time(200) ).to eq 5
+      it "returns correct word count when contents is multi word string" do
+        entry = DiaryEntry.new("meeting", "notes " * 10)
+        expect( entry.count_words ).to eq 10
+      end
     end
   end
 
-  context "given varying reading speeds and time available, returns correct chunk for given contents" do
-    it "returns entire contents if readable in time available" do
-      entry = DiaryEntry.new("meeting", "notes" + " notes" * 149)
-      expect( entry.reading_chunk(200, 1) ).to eq "notes" + " notes" * 149
-    end
+  describe "reading_time" do
+    context "given varying reading speeds, returns appropriate reading time for given contents" do
+      it "returns 1 for reading speed 200 wpm, word count 100" do
+        entry = DiaryEntry.new("meeting", "notes " * 100)
+        expect( entry.reading_time(200) ).to eq 1
+      end
 
-    it "returns readable chunk if contents not readable in time available" do
-      entry = DiaryEntry.new("meeting", "notes" + " notes" * 349)
-      expect( entry.reading_chunk(200, 1) ).to eq "notes" + " notes" * 199
+      it "returns 5 for reading speed 200 wpm, word count 1000" do
+        entry = DiaryEntry.new("meeting", "notes " * 1000)
+        expect( entry.reading_time(200) ).to eq 5
+      end
+
+      it "returns an error for a given reading speed of 0" do
+        entry = DiaryEntry.new("meeting", "notes")
+        expect{ entry.reading_time(0) }.to raise_error "Reading speed must be greater than zero."
+      end
     end
   end
 
-  context "when reading_chunk has previously been called, given varying reading speeds and time available, returns correct chunk for given contents" do
-    it "still returns the entire contents if readable in time available" do
-      entry = DiaryEntry.new("meeting", "notes" + " notes" * 149)
-      entry.reading_chunk(200, 1)
-      expect( entry.reading_chunk(200, 1) ).to eq "notes" + " notes" * 149
+  describe "reading_chunk" do
+    context "given varying reading speeds and time available, returns correct chunk for given contents" do
+      it "returns entire contents if readable in time available" do
+        entry = DiaryEntry.new("meeting", "notes" + " notes" * 149)
+        expect( entry.reading_chunk(200, 1) ).to eq "notes" + " notes" * 149
+      end
+
+      it "returns readable chunk if contents not readable in time available" do
+        entry = DiaryEntry.new("meeting", "notes" + " notes" * 349)
+        expect( entry.reading_chunk(200, 1) ).to eq "notes" + " notes" * 199
+      end
     end
 
-    it "returns next chunk of contents" do
-      entry = DiaryEntry.new("meeting", "notes" + " notes" * 199 + " *notes" * 250)
-      entry.reading_chunk(200, 1)
-      expect( entry.reading_chunk(200, 1) ).to eq "*notes" + " *notes" * 199
+    context "when reading_chunk has previously been called, given varying reading speeds and time available, returns correct chunk for given contents" do
+      it "still returns the entire contents if readable in time available" do
+        entry = DiaryEntry.new("meeting", "notes" + " notes" * 149)
+        entry.reading_chunk(200, 1)
+        expect( entry.reading_chunk(200, 1) ).to eq "notes" + " notes" * 149
+      end
+
+      it "returns next chunk of contents" do
+        entry = DiaryEntry.new("meeting", "notes" + " notes" * 199 + " *notes" * 250)
+        entry.reading_chunk(200, 1)
+        expect( entry.reading_chunk(200, 1) ).to eq "*notes" + " *notes" * 199
+      end
+
+      it "returns final chunk of contents" do
+        entry = DiaryEntry.new("meeting", "notes" + " notes" * 199 + " *notes" * 200 + " -notes" * 150)
+        entry.reading_chunk(200, 1)
+        entry.reading_chunk(200, 1)
+        expect( entry.reading_chunk(200, 1) ).to eq "-notes" + " -notes" * 149
+      end
     end
 
-    it "returns final chunk of contents" do
+    it "returns first chunk again after entire contents has been cycled" do
       entry = DiaryEntry.new("meeting", "notes" + " notes" * 199 + " *notes" * 200 + " -notes" * 150)
       entry.reading_chunk(200, 1)
       entry.reading_chunk(200, 1)
-      expect( entry.reading_chunk(200, 1) ).to eq "-notes" + " -notes" * 149
+      entry.reading_chunk(200, 1)
+      expect( entry.reading_chunk(200, 1) ).to eq "notes" + " notes" * 199
     end
-  end
 
-  it "returns first chunk again after entire contents has been cycled" do
-    entry = DiaryEntry.new("meeting", "notes" + " notes" * 199 + " *notes" * 200 + " -notes" * 150)
-    entry.reading_chunk(200, 1)
-    entry.reading_chunk(200, 1)
-    entry.reading_chunk(200, 1)
-    expect( entry.reading_chunk(200, 1) ).to eq "notes" + " notes" * 199
+    it "returns an error for a given reading speed of 0" do
+      entry = DiaryEntry.new("meeting", "notes")
+      expect{ entry.reading_chunk(0, 1) }.to raise_error "Reading speed and minutes must be greater than zero."
+    end
+
+    it "returns an error for given minutes of 0" do
+      entry = DiaryEntry.new("meeting", "notes")
+      expect{ entry.reading_chunk(200, 0) }.to raise_error "Reading speed and minutes must be greater than zero."
+    end
   end
 end
